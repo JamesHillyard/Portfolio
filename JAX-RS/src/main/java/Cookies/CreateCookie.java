b7c9df5ee5215b0a15a886acae29fb775a154ce1
James Hillyard <james.hillyard@payara.fish>
2021-04-13 12:47:52 +0100

    Added planet example with produces and consumes, with a Jersey client example of how to make GET and POST requests. Cookie examples added.
    Signed-off-by: James Hillyard <james.hillyard@payara.fish>

---
+++ JAX-RS/src/main/java/Cookies/CreateCookie.java
@@ -0,0 +1,20 @@
+package Cookies;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
+
+@Path("/newCookie")
+public class CreateCookie {
+
+    @POST
+    @Path("{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response createCookie(@PathParam("name") String name) {
+        return Response
+                .ok()
+                .cookie(new NewCookie("name", name))
+                .build();
+    }
+}
