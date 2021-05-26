1d6ebc188781766519ef0b7a337e43b189aa94f1
James Hillyard <james.hillyard@payara.fish>
2021-05-26 12:02:18 +0100

    General JPA Improvements
    Signed-off-by: James Hillyard <james.hillyard@payara.fish>

--- JavaEE/JPA/src/main/java/JPATutorial/EndpointExample/PersonResource.java
+++ JavaEE/JPA/src/main/java/JPATutorial/EndpointExample/PersonResource.java
@@ -1,8 +1,12 @@
 package JPATutorial.EndpointExample;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.*;
+import java.io.IOException;
 import java.util.List;
 
 @Stateless
@@ -18,10 +22,27 @@ public class PersonResource {
         return personDao.getAll();
     }
 
+    @GET
+    @Path("/{id}")
+    @Produces("application/json")
+    public Person byId(@PathParam("id") Long id){
+        return personDao.find(id);
+    }
+
+    //This won't allow us to submit a JSON array
     @POST
-    //@Consumes("application/json")
+    @Consumes("application/json")
     public void save(Person person) {
         personDao.save(person);
+    }
+
+    @POST
+    @Consumes("application/json")
+    @Path("/multiple")
+    public void saveMultiple(String persons) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Person> p = objectMapper.readValue(persons, new TypeReference<List<Person>>(){});
+        p.forEach(person -> personDao.save(person));
     }
 
     @PUT
