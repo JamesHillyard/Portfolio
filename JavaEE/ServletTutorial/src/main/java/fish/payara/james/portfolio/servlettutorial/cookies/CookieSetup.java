316a194f1a8afa091007aad94c292c7fb67a5008
James Hillyard <james.hillyard@payara.fish>
2021-06-08 14:30:21 +0100

    Updated ServletTutorial module for accurate naming convention packages.
    Signed-off-by: James Hillyard <james.hillyard@payara.fish>

---
+++ JavaEE/ServletTutorial/src/main/java/fish/payara/james/portfolio/servlettutorial/cookies/CookieSetup.java
@@ -0,0 +1,24 @@
+package fish.payara.james.portfolio.servlettutorial.cookies;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Create as a new cookie named "Basket" with the value "Bread"
+ */
+@WebServlet("/CreateCookie")
+public class CookieSetup extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Cookie basket = new Cookie("Basket", "Bread");
+		basket.setMaxAge(1800);
+		response.addCookie(basket);
+		response.sendRedirect("/CookieInfo");
+	}
+}
---
+++ JavaEE/ServletTutorial/src/main/java/fish/payara/james/portfolio/servlettutorial/servletutil/GetCookie.java
@@ -0,0 +1,22 @@
+package fish.payara.james.portfolio.servlettutorial.servletutil;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * A utility method to get a specific cookie by name
+ */
+public class GetCookie {
+
+	public static Cookie getCookie(HttpServletRequest request, String name) {
+	    Cookie[] cookies = request.getCookies();
+	    if (cookies != null) {
+	        for (Cookie cookie : cookies) {
+	            if (cookie.getName().equals(name)) {
+	                return cookie;
+	            }
+	        }
+	    }
+	    return null;
+	}
+}
