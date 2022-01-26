98fac2a8d68a08461ae8864fae841bb943880048
JamesHillyard <james.hillyard@payara.fish>
2022-01-26 15:51:47 +0000

    Websocket Tutorial
    Signed-off-by: JamesHillyard <james.hillyard@payara.fish>

---
+++ JavaEESamples/Websockets/src/main/java/james/websocket/MessageDecoder.java
@@ -0,0 +1,31 @@
+package james.websocket;
+
+import com.google.gson.Gson;
+
+import javax.websocket.Decoder;
+import javax.websocket.EndpointConfig;
+
+public class MessageDecoder implements Decoder.Text<Message> {
+
+    private static final Gson GSON = new Gson();
+
+    @Override
+    public Message decode(String s) {
+        return GSON.fromJson(s, Message.class);
+    }
+
+    @Override
+    public boolean willDecode(String s) {
+        return s != null;
+    }
+
+    @Override
+    public void init(EndpointConfig endpointConfig) {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
